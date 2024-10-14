01b7011055dfe23a13e559cfef75f551165237dd
ankitbisen <bisenankit21@gmail.com>
2024-10-15 01:11:05 +0530

    initial Commit

--- src/main/java/com/springSecurity/springsecsection1/controller/WelcomeController.java
+++ src/main/java/com/springSecurity/springsecsection1/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.springSecurity.springsecsection1.controller;
 
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,6 @@ public class WelcomeController {
     @GetMapping("/welcome")
     public String sayWelcome(){
         return "Welcome to Spring Application without security";
-    }
+   }
+
 }
