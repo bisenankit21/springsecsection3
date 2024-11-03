6154b464dd1bc42333ae6882e1d590cf70587770
ankitbisen <bisenankit21@gmail.com>
2024-11-03 21:42:02 +0530

    initial Commit

--- src/main/java/com/springSecurity/config/ProjectSecurityProdConfig.java
+++ src/main/java/com/springSecurity/config/ProjectSecurityProdConfig.java
@@ -1,3 +1,4 @@
+
 package com.springSecurity.config;
 
 import org.springframework.context.annotation.Bean;
@@ -11,9 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
-@Profile("!prod")
-public class ProjectSecurityConfig {
+@Profile("prod")
+public class ProjectSecurityProdConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
       //  http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
